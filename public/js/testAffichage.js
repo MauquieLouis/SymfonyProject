341d25c5ca8abd13dab29776c2b5dd4a2f38133b
Mod_loc <Mod_loc@D46-P03.D-Electronique.local>
2019-01-29 14:05:38 +0100

    Images+affichageDynamique

--- public/js/testAffichage.js
+++ public/js/testAffichage.js
@@ -1,25 +1,31 @@
 //- - - - - - -  T E S T   J A V A S C R I P T - - - - - - - - //
 
-var Text = [];
+//var Text = [];
+var i = -1;
 
- Text[0] = '<p>AFFICHAGE 1 => OUAIS C\'EST COOL</p>';
+var lis = document.getElementById("liste").getElementsByTagName("li");
+
+
+
+
+/* Text[0] = '<p>AFFICHAGE 1 => OUAIS C\'EST COOL</p>';
  Text[1] = '<p>AFFICHAGE 2 => SALUT T\'ES BO !</p>';
  Text[2] = '<p>AFFICHAGE 3 => J\'AIME LES TESLA</p>';
- Text[3] = '<p>AFFICHAGE 4 => LES AUTO C\'EST MIEUX QUAND IL Y A DU JAKITUNING</p>';
+ Text[3] = '<p>AFFICHAGE 4 => LES AUTO C\'EST MIEUX QUAND IL Y A DU JAKITUNING</p>';*/
  
  function Affichagetext(text)
  {
  	document.getElementById('Dynamique').innerHTML = text;
+ 	document.getElementById('btn').remove();
  }
- var i = -1;
+
 
  function AffichageDyn()
  {
- 		i++;
- 		if(i>=4)
+ 		//i=4 ?  0 : i++;
+ 		if(++i>= lis.length )
  		{
  			i=0;
  		}
- 		var texte = 'Text' + i.toString();
- 		Affichagetext(Text[i]);		
+ 		Affichagetext(lis[i].innerHTML);		
  }
